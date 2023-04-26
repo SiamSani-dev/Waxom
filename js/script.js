0bca3b3c565d99b6f72807c5ab15db49b7b62786
SiamSani-dev <siam.sani7@gmail.com>
2023-04-26 19:36:43 +0600

    I've done my project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+          // =======COUNTER SLIDER PART START========
+  
+          $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+  
+  
+          // =======COUNTER SLIDER PART END========
+  
+
+      })
